0f1cb891550ce3a11c00acea1d3755d5957cb512
Nick Howes <n.howes@warwick.ac.uk>
2015-05-22 09:29:15 +0100

    Initial semi-working inventory script.
    Handles --list and --host mode, but haven't actually
    tested it against Ansible yet to check that it works
    fully. May be some bugs relating to FQDN/clientcert
    mismatch where those aren't the same.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem "puppetdb-ruby"
+gem 'httparty', '0.13.2'
---
+++ inventory.rb
@@ -0,0 +1,134 @@
+#!/usr/bin/env ruby
+
+require 'puppetdb'
+require 'yaml'
+require 'json'
+require 'optparse'
+
+class Inventory
+
+  def initialize(settings)
+    @client = PuppetDB::Client.new({
+      :server => settings['server'],
+      :pem => {
+        'cert'    => settings['ssl_cert'],
+        'key'     => settings['ssl_key'],
+        'ca_file' => settings['ssl_ca']
+      }
+    })
+  end
+    
+    
+  def fetch_host_by_fqdn(fqdn)
+    response = @client.request('facts', [:'and',
+      [:'=', 'name', 'fqdn'],
+      [:'=', 'value', fqdn]
+    ])
+    (response.data[0] || {})['certname']
+  end
+
+  def fetch_hosts_metadata()
+    response = @client.request('facts', ['~', 'name', '.*'])
+    what = response.data.group_by { |fact|
+      fact['certname'] 
+    }.map { |certname, facts|
+      [
+        certname, 
+        Hash[facts.map { |fact| [ fact['name'], fact['value'] ] }]
+      ]
+    }
+    Hash[what]
+  end
+
+  def process(mode, hostname=nil)
+    
+    data = {}
+    if mode == :list
+    
+      nodes_response = @client.request('nodes', [:'~','name','.*'])
+      nodes = nodes_response.data
+      data = {
+        'all' => {
+          'hosts' => nodes.map { |res| res['name'] }.uniq
+        },
+        '_meta' => {
+          'hostvars' => fetch_hosts_metadata() 
+        }
+      }
+      
+      response = @client.request(
+        'resources',
+        [:and,
+          ['=', 'type', 'Nodes::Exported_metadata']
+        ]
+      )
+      
+      items = response.data
+      
+      items.each do |item|
+        host = item['certname']
+        params = item['parameters']
+        app = params['node_app']
+        tier = params['node_tier']
+        deployment = params['node_deployment']
+        group_name = "#{deployment}_#{app}_#{tier}"
+        data[group_name] ||= {}
+        data[group_name]['hosts'] ||= []
+        data[group_name]['hosts'].push(host)
+      end
+    
+    elsif mode == :host
+    
+      clientcert = fetch_host_by_fqdn(hostname)
+
+      if clientcert
+    
+        response = @client.request("nodes/#{clientcert}/facts", [:'and',
+          [:'~', 'name', '.*'],
+        ])
+        facts = response.data
+        data[hostname] = Hash[facts.map { |fact|
+          [ fact['name'], fact['value'] ]
+        }]
+
+      end
+    
+    else
+      data['error'] = 'No mode selected'
+    end
+    
+    
+    puts data.to_json
+    
+    if data['error']
+      exit 1
+    end
+
+  end
+
+end
+
+
+SETTINGS_FILE = 'settings.yml'
+
+settings = YAML.load(File.open(SETTINGS_FILE))
+
+inv = Inventory.new(settings)
+
+mode = nil
+hostname = nil
+OptionParser.new do |opts|
+  opts.on('-l', '--list', 'List all hosts') do |l|
+    mode = :list
+  end
+  opts.on('-h', '--host HOST', 'Single host detail') do |host|
+    mode = :host
+    hostname = host
+  end
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+end.parse!(ARGV)
+
+inv.process(mode, hostname)
